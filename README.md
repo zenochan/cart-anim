fd46cea4e001f73ca2bfdbd24d09af56242b11db
陈治谋 <513500085@qq.com>
2017-03-02 22:52:11 +0800

    upload to maven

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 购物车动画 - 贝塞尔曲线形式
 
 ![](./cart-anim.gif)
+
+## 使用
+
+```groovy
+repositories {
+  //...
+  maven { url 'http://maven.mjtown.cn/' }
+}
+
+dependencies{
+  compile 'name.zeno:cart-anim:1.0.0';
+}
+```
--- library/src/main/java/zeno/name/animcart/BezierCurve.java
+++ library/src/main/java/zeno/name/animcart/BezierCurve.java
@@ -14,8 +14,8 @@ public class BezierCurve
    */
   public static PointF bezier(@FloatRange(from = 0, to = 1) float t, PointF point0, PointF point1, PointF point2)
   {
-    float oneMinusT = 1.0f - t;
-    PointF point = new PointF();
+    float  oneMinusT = 1.0f - t;
+    PointF point     = new PointF();
     point.x = oneMinusT * oneMinusT * point0.x
         + 2 * t * oneMinusT * point1.x
         + t * t * point2.x;
@@ -30,8 +30,8 @@ public class BezierCurve
    */
   public static PointF bezier(@FloatRange(from = 0, to = 1) float t, PointF point0, PointF point1, PointF point2, PointF point3)
   {
-    float oneMinusT = 1.0f - t;
-    PointF point = new PointF();
+    float  oneMinusT = 1.0f - t;
+    PointF point     = new PointF();
     point.x = oneMinusT * oneMinusT * oneMinusT * (point0.x)
         + 3 * oneMinusT * oneMinusT * t * (point1.x)
         + 3 * oneMinusT * t * t * (point2.x)
